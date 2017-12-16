86a7db67c27eaf2339b6680ac6a9c6de84309523
keti <katarina1d98@gmail.com>
2017-12-16 12:23:46 -0600

    dbut

--- db_utils.php
+++ db_utils.php
@@ -13,27 +13,23 @@
   }
 
   function getAnswers(){
-
     return array( array(
-
       "TextOdg" => "neki odgovor1",
       "AutorOdg" => "Ime autora1"
-    )
+    ),
     array(
-
       "TextOdg" => "neki odgovor2",
       "AutorOdg" => "Ime autora2"
-    )
+    ),
     array(
-
       "TextOdg" => "neki odgovor3",
       "AutorOdg" => "Ime autora3"
-    )
+    ),
     array(
-
       "TextOdg" => "neki odgovor4",
       "AutorOdg" => "Ime autora4"
-    ));
+    )
+  );
   }
 
  ?>
