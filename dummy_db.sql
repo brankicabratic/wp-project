a8a5256c9e01b85dcc49619165cc7cb969b85073
MilosVujasinovic <milos.vujasinovic@gmail.com>
2017-12-16 14:51:59 +0100

    Dummy data for database

---
+++ dummy_db.sql
@@ -0,0 +1,18 @@
+INSERT INTO User(username, password, fullname, avatar, email, major, enrollmentyear) VALUES ("perica", "nest@#!@#!@", "Pera Peric", "nesto.jpg", "perica@gmail.com", "Racunarske nauke", 2017);
+INSERT INTO User(username, password, fullname, avatar, email, major, enrollmentyear) VALUES ("staraR", "nest@#!@#!@", "Stara Rosa", "nesto.jpg", "rosa@gmail.com", "Informacione tehnologije", 2016);
+
+INSERT INTO Tag(name) VALUES("PHP");
+INSERT INTO Tag(name) VALUES("HTML");
+INSERT INTO Tag(name) VALUES("Math");
+INSERT INTO Tag(name) VALUES("Javascript");
+INSERT INTO Tag(name) VALUES("CSS");
+INSERT INTO Tag(name) VALUES("Big Data");
+
+INSERT INTO Post(Header, Content, PostingTime, Author) VALUES ("How to use Dictionary in Swift?
+", "I created Dictionary like below, and I'd like to make a tableView using this book list.", date("16-12-2017"), 1);
+INSERT INTO Post(Header, Content, PostingTime, Author) VALUES ("Value For Dict - Python", "I have a code to get a specific value from yahoo API. The problem is it matched the IF statement but it returns None and also again its going to the else loop for some reason. Can some one help me out pls. Very new to python.", date("15-01-2018"), 1);
+
+INSERT INTO PostTags VALUES (1, 1);
+INSERT INTO PostTags VALUES (1, 6);
+INSERT INTO PostTags VALUES (2, 3);
+INSERT INTO PostTags VALUES (2, 4);
