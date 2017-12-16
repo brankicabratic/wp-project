92790553ea001169114ac01fdf86d41bca1fcc80
Ognjen Vuckovic <ogivuk2@gmail.com>
2017-12-16 11:31:55 +0100

    Auto stash before merge of "master" and "origin/master"

--- post.php
+++ post.php
@@ -1,10 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1> <?php echo "getHeading"; ?> </h1>
-	<div align="right">  <a href="putpost.php"><button>Postavi pitanje</button></a></div>
-</body>
-</html>
+<?php require_once("parts.php"); ?>
+<html> 
+<head> 
+  <title>getHeding</title> 
+</head> 
+<body> 
+	<?php printHeader();?>
+  <h1> <?php echo "getHeading"; ?> </h1> 
+  <div align="right">   
+    <a href="putpost.php"><button>Postavi pitanje</button></a><br> 
+    asked: Datum php<br> 
+    viewed: Koliko je vidjeno php<br> 
+    active: Kad je zadnji put odgovarano<br> 
+  </div> 
+  <div align="center"> 
+    <p> ukratko pitanje <br> 
+    Kod pitanja<br> 
+    Jos pitanja<br> 
+    Tagovi<br> 
+    Korisnik koji je objavio<br> 
+    </p> 
+ 
+  </div> 
+  <div align="center"> 
+    <p> ukratko odgovor <br> 
+    Kod odgovor<br> 
+    Jos odgovor</p> 
+    Korisnik koji je objavio<br></p> 
+  </div> 
+ 
+</body> 
