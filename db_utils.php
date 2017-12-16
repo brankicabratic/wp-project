3d1be878dd547268c8c42faa3760da6df8247e71
keti <katarina1d98@gmail.com>
2017-12-16 11:48:11 -0600

    db utils

---
+++ db_utils.php
@@ -0,0 +1,24 @@
+<?php
+  function getPost($id){
+    return array(
+      "Naslov"=>"Neki naslov",
+      "Text"=> "Neki tekst",
+      "Autor"=>"ime autora"
+      "Datum"=> "12.12.2017"
+    );
+  }
+
+  function getTags(){
+      return array(
+        "tag"=>"neki tag"
+      );
+  }
+
+  function getReply(){
+    return  array(
+      "TextOdg" => "neki odgovor"
+      "AutorOdg" => "Ime autora"
+     );
+  }
+  
+ ?>
