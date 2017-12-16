f059d15ce1081946164a6f4decec9e575a6ca88a
Ognjen Vuckovic <ogivuk2@gmail.com>
2017-12-16 10:59:54 +0100

    Post

--- post.php
+++ post.php
@@ -1,2 +1,10 @@
-<h1> <?php echo "getHeading"; ?> </h1>
-<div align="right">  <a href="putpost.php"><button>Postavi pitanje</button></a></div>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1> <?php echo "getHeading"; ?> </h1>
+	<div align="right">  <a href="putpost.php"><button>Postavi pitanje</button></a></div>
+</body>
+</html>
