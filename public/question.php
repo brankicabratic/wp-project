cb5459b5b26ce242f6bbd7d495d398c68543536f
mvujas <milos.vujasinovic.98@gmail.com>
2018-01-27 13:16:48 +0100

    Answers section added

--- public/question.php
+++ public/question.php
@@ -47,27 +47,91 @@
 
           <div class="row">
             <div class="col-1 d-none d-lg-block scroll-top-answers-side">
-              <span class="arrow invisible back-to-top-button"><a title="Back to top" href="#answers"><i class="fas fa-arrow-circle-up"></i></a></span>
+              <span class="arrow invisible back-to-top-button"><a title="Back to top" href="#the-question"><i class="fas fa-arrow-circle-up"></i></a></span>
             </div>
             <div class="col-lg-11">
+              <h4>Ostavi odgovor:</h4>
+              <div class="write-answer">
+                <form method="post" id="answer-form">
+                    <div class="text-formating-tools">
+                      <span class="tool" onclick="tools.delete()"><i class="fas fa-trash-alt"></i></span>
+                      <span class="tool" onclick="tools.addCustomTag(' [superscript]', '[/superscript] ')"><i class="fas fa-superscript"></i></span>
+                      <span class="tool" onclick="tools.addCustomTag('[code lang=\'html\']\n', '\n[/code]')"><i class="fas fa-code"></i></span>
+                    </div>
+                    <textarea name="answer-content" spellcheck="false" placeholder="Odgovor"></textarea>
+                    <div class="submit-group">
+                      <input type="submit" class="btn btn-primary" name="" value="Odgovori">
+                    </div>
+                </form>
+              </div>
+
               <div id="answers">
-                <h4>Ostavi odgovor:</h4>
+
                 <div class="answer">
-                  <form method="post">
-                      <div class="text-formating-tools">
-                        <span class="tool"><i class="fas fa-trash-alt"></i></span>
-                        <span class="tool"><i class="fas fa-superscript"></i></span>
-                      </div>
-                      <textarea name="answer-content" spellcheck="false" placeholder="Odgovor"></textarea>
-                      <div class="submit-group">
-                        <input type="submit" class="btn btn-primary" name="" value="Odgovori">
-                      </div>
-                  </form>
+                  <div class="content">
+                    Another solution that I have developed and might be worth considering: http://codility.com/demo/results/demoM658NU-DYR/
+                  </div>
+                  <div class="footer">
+                    <div class="aligned-right">
+                      <span class="score">
+                        <span class="reaction dislike active"><i class="fas fa-caret-left"></i></span>
+                        <span class="actual-score">0</span>
+                        <span class="reaction like"><i class="fas fa-caret-right"></i></span>
+                      </span>
+                      Odgovorio <a href="#">peraPeric</a> 25. 01. 2018. u 14:52
+                    </div>
+                  </div>
                 </div>
 
-                asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>asdasdasdssad <br>
+                <div class="answer">
+                  <div class="content">
+                    Another solution that I have developed and might be worth considering: http://codility.com/demo/results/demoM658NU-DYR/
+                  </div>
+                  <div class="footer">
+                    <div class="aligned-right">
+                      <span class="score">
+                        <span class="reaction dislike active"><i class="fas fa-caret-left"></i></span>
+                        <span class="actual-score">0</span>
+                        <span class="reaction like"><i class="fas fa-caret-right"></i></span>
+                      </span>
+                      Odgovorio <a href="#">peraPeric</a> 25. 01. 2018. u 14:52
+                    </div>
+                  </div>
+                </div>
 
-                <span class="back-to-top-button d-lg-none"><a title="Back to top" href="#answers"><i class="fas fa-arrow-circle-up"></i></a></span>
+                <div class="answer">
+                  <div class="content">
+                    Another solution that I have developed and might be worth considering: http://codility.com/demo/results/demoM658NU-DYR/
+                  </div>
+                  <div class="footer">
+                    <div class="aligned-right">
+                      <span class="score">
+                        <span class="reaction dislike active"><i class="fas fa-caret-left"></i></span>
+                        <span class="actual-score">0</span>
+                        <span class="reaction like"><i class="fas fa-caret-right"></i></span>
+                      </span>
+                      Odgovorio <a href="#">peraPeric</a> 25. 01. 2018. u 14:52
+                    </div>
+                  </div>
+                </div>
+
+                <div class="answer">
+                  <div class="content">
+                    Another solution that I have developed and might be worth considering: http://codility.com/demo/results/demoM658NU-DYR/
+                  </div>
+                  <div class="footer">
+                    <div class="aligned-right">
+                      <span class="score">
+                        <span class="reaction dislike active"><i class="fas fa-caret-left"></i></span>
+                        <span class="actual-score">0</span>
+                        <span class="reaction like"><i class="fas fa-caret-right"></i></span>
+                      </span>
+                      Odgovorio <a href="#">peraPeric</a> 25. 01. 2018. u 14:52
+                    </div>
+                  </div>
+                </div>
+
+                <span class="back-to-top-button d-lg-none"><a title="Back to top" href="#the-question"><i class="fas fa-arrow-circle-up"></i></a></span>
               </div>
             </div>
           </div>
@@ -102,7 +166,7 @@
         var arrowContainerHeight = arrowContainer.height();
         var arrowContainerEnd = arrowContainerStart + arrowContainerHeight;
 
-        if(arrowContainerHeight < 350 || windowTop < arrowContainerStart)
+        if(arrowContainerHeight < 500 || windowTop < arrowContainerStart)
           arrow.addClass("invisible");
         else {
           var dif = windowTop - arrowContainerStart;
@@ -113,6 +177,49 @@
           arrow.removeClass("invisible");
         }
       };
+
+      //
+      var answerInput = document.querySelector("#answer-form textarea");
+      var answerInputFocused = false;
+      answerInput.addEventListener("focus", function() {
+        answerInputFocused = true;
+      });
+      answerInput.addEventListener("blur", function() {
+        setTimeout(function() {
+          answerInputFocused = false;
+        }, 100);
+      });
+
+
+      var getTextAreaSelection = function() {
+        if(!answerInputFocused)
+          return {
+            pre: answerInput.value,
+            selected: '',
+            post: ''
+          };
+
+        var start = answerInput.selectionStart;
+        var finish = answerInput.selectionEnd;
+
+        return {
+          pre: answerInput.value.substring(0, start),
+          selected: answerInput.value.substring(start, finish),
+          post: answerInput.value.substring(finish, answerInput.value.length)
+        }
+      }
+
+      var tools = {
+        delete: function() {
+          textAreaVal = getTextAreaSelection();
+          answerInput.value = textAreaVal.pre + textAreaVal.post;
+        },
+        addCustomTag: function(preTag, postTag) {
+          textAreaVal = getTextAreaSelection();
+          answerInput.value = textAreaVal.pre + preTag + textAreaVal.selected + postTag + textAreaVal.post;
+        }
+      };
+
 
       $(window).scroll(function() {
         calculateArrowToTopStyle();
